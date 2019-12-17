task 2 fix - average over 10 iterations

main.py
@@ -1,5 +1,5 @@
 import networkx as nx
-import matplotlib as mpl
+from datetime import datetime
 import numpy as np
 from utils import infection_time, create_bins, plot_avg_prevalence
 
@@ -14,8 +14,8 @@ def main():
     # task 1 #
     ##########
 
-    # infection_times, infection_list = infection_time(event_data, 1, 0)
-    # print("Node 41 infection time: " + str(infection_times[41]))
+    infection_times, infection_list = infection_time(event_data, 1, 0)
+    print("Node 41 infection time: " + str(infection_times[41]) +" "+ str(datetime.fromtimestamp(infection_times[41])))
 
     # animation of the infection
     # visualize_si(np.array(infection_list), save_fname="si_viz_example.mp4")
@@ -25,10 +25,14 @@ def main():
     ##########
 
     infection_prob = [0.01, 0.05, 0.1, 0.5, 1.0]
-    infection_times_list = []
+    infection_times_list_avg = []
+    infection_times_list_probs = []
     for prob in infection_prob:
-        _, infection_list = infection_time(event_data, prob, 0)
-        infection_times_list.append(infection_list)
+        for i in range(10):
+            _, infection_list = infection_time(event_data, prob, 0)
+            infection_times_list_avg.append(infection_list)
+        infection_times_list_probs.append(infection_times_list_avg)
+        infection_times_list_avg = []
 
 
     min_timestemp = min(event_data, key=lambda item:item["StartTime"])[2]
@@ -36,7 +40,7 @@ def main():
     n_bins = 50
     bins = create_bins(min_timestemp, max_timestemp, n_bins)
 
-    plot_avg_prevalence(infection_times_list, infection_prob, n_nodes, bins)
+    plot_avg_prevalence(infection_times_list_probs, infection_prob, n_nodes, bins)
 
 
 

utils.py
@@ -3,7 +3,7 @@ from random import random, seed
 import matplotlib.pyplot as plt
 from scipy.stats import binned_statistic
 import datetime as dt
-
+from statistics import mean
 
 def infection_time(event_list, p, seed_node):
     """
@@ -21,7 +21,7 @@ def infection_time(event_list, p, seed_node):
 
     """
 
-    seed(12)
+    # seed(12)
 
     infection_times = {}
     found_seed = False
@@ -76,29 +76,35 @@ def plot_avg_prevalence(infection_times_list, infection_prob, n_nodes, bins):
     -------
 
     """
-    # TODO:  average over these values over the 10 iterations
     fig = plt.figure()
     ax = fig.add_subplot(111)
     bin_centers = (bins[:-1] + bins[1:]) / 2
     dateconv = np.vectorize(dt.datetime.fromtimestamp)
     date = dateconv(bin_centers)
+    prevalences = []
     for list, prob in zip(infection_times_list, infection_prob):
-        counts, _, _ = binned_statistic(
-            x=list,
-            values=list,
-            bins=bins,
-            statistic='count')
+        for l in list:
+            counts, _, _ = binned_statistic(
+                x=l,
+                values=l,
+                bins=bins,
+                statistic='count')
 
-        cum_counts = np.cumsum(counts)
-        avg_prevalence = cum_counts / n_nodes
+            cum_counts = np.cumsum(counts)
+            prevalence = cum_counts / n_nodes
+            prevalences.append(prevalence)
+
+        avg_prevalence = np.array(prevalences)
+        avg_prevalence = avg_prevalence.mean(0)
+
         ax.plot(date, avg_prevalence, label=prob)
-
+        prevalences = []
         # ax.get_xaxis().get_major_formatter().set_useOffset(False)
         # ax.get_xaxis().get_major_formatter().set_scientific(False)
 
     ax.legend()
     plt.suptitle(r'Averaged prevalence of the disease for each of the infection probabilities')
-    ax.set_xlabel(r'date')
+    ax.set_xlabel(r'time')
     ax.set_ylabel(r'averaged prevalence $\rho(t)$')
 
     fig.autofmt_xdate(bottom=0.2, rotation=20, ha='right')